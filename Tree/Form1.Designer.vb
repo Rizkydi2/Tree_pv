4015fa7a7c489f74c94d53724f782f86cb54ae9e
Rizkydi2 <Agus Imansyah@DESKTOP-HLFP9PP>
2025-06-05 09:40:33 +0700

    commit 1

---
+++ Tree/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        Label4 = New Label()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        TextBox3 = New TextBox()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        GroupBox2 = New GroupBox()
+        ListBox2 = New ListBox()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(ListBox1)
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(TextBox3)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Location = New Point(12, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(978, 289)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "[Data Entry]"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(423, 30)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(97, 25)
+        Label4.TabIndex = 8
+        Label4.Text = "Summary :"
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.Location = New Point(423, 58)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(539, 204)
+        ListBox1.TabIndex = 7
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(21, 197)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(330, 40)
+        Button1.TabIndex = 7
+        Button1.Text = "Submit"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(26, 58)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(325, 31)
+        TextBox3.TabIndex = 6
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(201, 142)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(201, 114)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(55, 25)
+        Label3.TabIndex = 4
+        Label3.Text = "UAS :"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(26, 142)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(146, 31)
+        TextBox1.TabIndex = 4
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(21, 30)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(68, 25)
+        Label1.TabIndex = 2
+        Label1.Text = "Name :"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(26, 114)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(52, 25)
+        Label2.TabIndex = 3
+        Label2.Text = "UTS :"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(ListBox2)
+        GroupBox2.Controls.Add(RadioButton2)
+        GroupBox2.Controls.Add(RadioButton1)
+        GroupBox2.Location = New Point(12, 317)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(978, 179)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "[Filtering]"
+        ' 
+        ' ListBox2
+        ' 
+        ListBox2.FormattingEnabled = True
+        ListBox2.Location = New Point(413, 30)
+        ListBox2.Name = "ListBox2"
+        ListBox2.Size = New Size(549, 129)
+        ListBox2.TabIndex = 8
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(205, 57)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(141, 29)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Lowest Score"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(25, 57)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(147, 29)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Highest Score"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1024, 567)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+
+End Class
---
+++ Tree/Form1.vb
@@ -0,0 +1,156 @@
+﻿Public Class Form1
+    Public Structure Student
+        Public Name As String
+        Public UTS As Double
+        Public UAS As Double
+        Public Total As Double
+    End Structure
+
+    Private students As New List(Of Student)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InitializeData()
+        DisplayData(students)
+    End Sub
+
+    Private Sub InitializeData()
+        students.Add(New Student With {.Name = "Ahmad", .UTS = 85, .UAS = 90, .Total = 175})
+        students.Add(New Student With {.Name = "Budi", .UTS = 78, .UAS = 82, .Total = 160})
+        students.Add(New Student With {.Name = "Citra", .UTS = 92, .UAS = 88, .Total = 180})
+        students.Add(New Student With {.Name = "Deni", .UTS = 65, .UAS = 70, .Total = 135})
+        students.Add(New Student With {.Name = "Eka", .UTS = 88, .UAS = 94, .Total = 182})
+        students.Add(New Student With {.Name = "Farah", .UTS = 72, .UAS = 76, .Total = 148})
+        students.Add(New Student With {.Name = "Gilang", .UTS = 95, .UAS = 89, .Total = 184})
+        students.Add(New Student With {.Name = "Hani", .UTS = 80, .UAS = 85, .Total = 165})
+    End Sub
+
+    Private Sub DisplayData(studentList As List(Of Student))
+        ListBox1.Items.Clear()
+
+        ListBox1.Items.Add("Name" & vbTab & vbTab & "UTS" & vbTab & "UAS" & vbTab & "Total")
+        ListBox1.Items.Add("----------------------------------------")
+
+        For Each student In studentList
+            Dim displayText As String = student.Name & vbTab & vbTab &
+                                      student.UTS.ToString() & vbTab &
+                                      student.UAS.ToString() & vbTab &
+                                      student.Total.ToString()
+            ListBox1.Items.Add(displayText)
+        Next
+
+        UpdateSummary(studentList)
+    End Sub
+
+    Private Sub UpdateSummary(studentList As List(Of Student))
+        ListBox2.Items.Clear()
+
+        If studentList.Count > 0 Then
+            Dim avgUTS As Double = studentList.Average(Function(s) s.UTS)
+            Dim avgUAS As Double = studentList.Average(Function(s) s.UAS)
+            Dim avgTotal As Double = studentList.Average(Function(s) s.Total)
+
+            ListBox2.Items.Add("Summary Statistics:")
+            ListBox2.Items.Add("------------------")
+            ListBox2.Items.Add("Average UTS: " & Math.Round(avgUTS, 2).ToString())
+            ListBox2.Items.Add("Average UAS: " & Math.Round(avgUAS, 2).ToString())
+            ListBox2.Items.Add("Average Total: " & Math.Round(avgTotal, 2).ToString())
+            ListBox2.Items.Add("Total Students: " & studentList.Count.ToString())
+        End If
+    End Sub
+
+    Private Sub Submit_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim newStudent As New Student
+
+            newStudent.Name = TextBox3.Text.Trim()
+
+            If String.IsNullOrEmpty(newStudent.Name) Then
+                MessageBox.Show("Silakan masukkan nama mahasiswa", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Return
+            End If
+
+            If Not Double.TryParse(TextBox1.Text, newStudent.UTS) OrElse newStudent.UTS < 0 OrElse newStudent.UTS > 100 Then
+                MessageBox.Show("Masukkan nilai UTS yang valid (0-100)", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                TextBox1.Focus()
+                Return
+            End If
+
+            If Not Double.TryParse(TextBox2.Text, newStudent.UAS) OrElse newStudent.UAS < 0 OrElse newStudent.UAS > 100 Then
+                MessageBox.Show("Masukkan nilai UAS yang valid (0-100)", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                TextBox2.Focus()
+                Return
+            End If
+
+            ' Hitung total
+            newStudent.Total = newStudent.UTS + newStudent.UAS
+
+            students.Add(newStudent)
+
+            TextBox3.Clear() ' Clear nama
+            TextBox1.Clear() ' Clear UTS
+            TextBox2.Clear() ' Clear UAS
+
+            If RadioButton1.Checked Then
+                DisplayHighestScores()
+            ElseIf RadioButton2.Checked Then
+                DisplayLowestScores()
+            Else
+                DisplayData(students)
+            End If
+
+            MessageBox.Show("Data mahasiswa berhasil ditambahkan!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show("Error menambahkan data: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If RadioButton1.Checked Then
+            DisplayHighestScores()
+        End If
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        If RadioButton2.Checked Then
+            DisplayLowestScores()
+        End If
+    End Sub
+
+    Private Sub DisplayHighestScores()
+        Dim topStudents = students.OrderByDescending(Function(s) s.Total).Take(5).ToList()
+        DisplayData(topStudents)
+
+        ListBox2.Items.Clear()
+        ListBox2.Items.Add("Top 5 Highest Scores:")
+        ListBox2.Items.Add("-------------------")
+
+        For i As Integer = 0 To Math.Min(topStudents.Count - 1, 4)
+            ListBox2.Items.Add($"{i + 1}. {topStudents(i).Name} - {topStudents(i).Total}")
+        Next
+    End Sub
+
+    Private Sub DisplayLowestScores()
+        Dim bottomStudents = students.OrderBy(Function(s) s.Total).Take(5).ToList()
+        DisplayData(bottomStudents)
+
+        ListBox2.Items.Clear()
+        ListBox2.Items.Add("Top 5 Lowest Scores:")
+        ListBox2.Items.Add("-------------------")
+
+        For i As Integer = 0 To Math.Min(bottomStudents.Count - 1, 4)
+            ListBox2.Items.Add($"{i + 1}. {bottomStudents(i).Name} - {bottomStudents(i).Total}")
+        Next
+    End Sub
+
+    Private Sub ResetView()
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        DisplayData(students)
+    End Sub
+
+    Private Sub ButtonReset_Click(sender As Object, e As EventArgs)
+        ResetView()
+    End Sub
+
+End Class
